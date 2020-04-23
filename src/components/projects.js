37282c968beb589d05a6227268541c55d69a0eb8
saagarparikh <saagarparikh24@gmail.com>
2020-04-22 22:31:11 -0700

    Github Pages Deploy

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,7 @@
 import React, { Component, Image } from 'react';
 import { Grid, Cell, Card, CardTitle, CardText, CardMenu, IconButton } from 'react-mdl';
 import arrow from './arrow.png'
+import missionforward from './missionforward.png'
 
 class Projects extends Component{
   handleScroll(e) {
@@ -18,7 +19,7 @@ class Projects extends Component{
           <a href="#top"><button id="btnScrollToTop"><img src={arrow}/></button></a>
           <Grid container spacing={24} direction="column" align="center" id = "top">
             <Cell col={12}>
-            <a href="https://github.com/saagarparikh/MissionForward"><img className = "profile" src="missionforward.png"/> </a>
+            <a href="https://github.com/saagarparikh/MissionForward"><img className = "profile" src={missionforward}"/> </a>
             <h4> Mission Forward is a project that helps soon-to-be college students navigate their personal and professional lives after HS.</h4>
             </Cell>
             <Cell col={12}>
