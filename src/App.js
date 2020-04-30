8a80e734eaccd5d4790da8fa1db20704b35cc63e
saagarparikh <saagarparikh24@gmail.com>
2020-04-29 22:47:16 -0700

    Used state for active tab in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,51 @@ import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import { Link, NavLink } from 'react-router-dom';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeHome: true,
+      activeImages: false,
+      activeVideos: false,
+      activeProjects: false,
+    };
+  }
+
+  setActiveHome = (e) => {
+    this.setState({
+      activeHome: true,
+      activeImages: false,
+      activeVideos: false,
+      activeProjects: false,
+    });
+  }
+
+  setActiveImages = (e) => {
+    this.setState({
+      activeHome: false,
+      activeImages: true,
+      activeVideos: false,
+      activeProjects: false,
+    });
+  }
+
+  setActiveVideos = (e) => {
+    this.setState({
+      activeHome: false,
+      activeImages: false,
+      activeVideos: true,
+      activeProjects: false,
+    });
+  }
+
+  setActiveProjects = (e) => {
+    this.setState({
+      activeHome: false,
+      activeImages: false,
+      activeVideos: false,
+      activeProjects: true,
+    });
+  }
   render() {
     return (
       <div>
@@ -12,18 +57,18 @@ class App extends Component {
             <Layout>
                 <Header title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Saagar Parikh</Link>} scroll>
                     <Navigation>
-                      <NavLink exact activeClassName = "active-nav-tab" to="/home">Home</NavLink>
-                      <NavLink exact activeClassName = "active-nav-tab" to="/images">Images</NavLink>
-                      <NavLink exact activeClassName = "active-nav-tab" to="/videos">Videos</NavLink>
-                      <NavLink exact activeClassName = "active-nav-tab" to="/projects">Projects</NavLink>
+                      <NavLink exact className = {this.state.activeHome && 'active-nav-tab'} onClick={this.setActiveHome} to="/home">Home</NavLink>
+                      <NavLink exact className = {this.state.activeImages && 'active-nav-tab'} onClick={this.setActiveImages} to="/images">Images</NavLink>
+                      <NavLink exact className = {this.state.activeVideos && 'active-nav-tab'} onClick={this.setActiveVideos} to="/videos">Videos</NavLink>
+                      <NavLink exact className = {this.state.activeProjects && 'active-nav-tab'} onClick={this.setActiveProjects} to="/projects">Projects</NavLink>
                     </Navigation>
                 </Header>
                 <Drawer title="Title">
                     <Navigation>
-                        <a href="/home">Home</a>
-                        <a href="/images">Images</a>
-                        <a href="/videos">Vidoes</a>
-                        <a href="/projects">Projects</a>
+                      <NavLink exact to="/home">Home</NavLink>
+                      <NavLink exact to="/images">Images</NavLink>
+                      <NavLink exact to="/videos">Videos</NavLink>
+                      <NavLink exact to="/projects">Projects</NavLink>
                     </Navigation>
                 </Drawer>
                 <Content>
